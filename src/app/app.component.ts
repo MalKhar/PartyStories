7d974d0b3e66f8672b0e6a35e6be3669c8e22901
Lucas Borgesgit <lucasborgesmoraes@gmail.com>
2023-11-07 19:42:52 -0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TaskService } from 'src/services/task.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'task';
+
+  constructor(
+   private taskService: TaskService
+  ){
+
+  }
+
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.taskService.getTasks().subscribe((res)=>{
+      console.log(res)
+    })
+  }
 }
+
+
